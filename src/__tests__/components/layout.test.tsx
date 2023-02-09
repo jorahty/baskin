de6b28aba5dac21052a2cb7be355fa656180cd31
José Chavez <josmchavz@gmail.com>
2023-02-09 14:57:42 -0800

    #30

--- src/__tests__/components/layout.test.tsx
+++ src/__tests__/components/layout.test.tsx
@@ -3,7 +3,13 @@ import { render } from '@testing-library/react'
 import Layout from "../../components/Layout";
 
 const renderView = async () => {
-  render(<Layout />)
+  render(
+    <>
+      <Layout>
+        <p>Children components go here.</p>
+      </Layout>
+    </>
+  )
 };
 
 test('Layout Renders', async () => {
