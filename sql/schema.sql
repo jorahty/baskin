a714ced52f4f38a0969be1e3e7e94e984ff0b9f9
nreusken <nreusken@hotmail.com>
2023-02-08 23:16:13 -0800

    Created public table and data for it(#15)

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,5 @@ drop schema public cascade;
 create schema public;
 
 CREATE TABLE member (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), email VARCHAR(255), data jsonb);
+
+CREATE TABLE product (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), mid UUID REFERENCES member(id) ON DELETE CASCADE, data jsonb);
