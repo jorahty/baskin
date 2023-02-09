099c1cff8399ba595c51e163bc54f0d8a18514e6
nreusken <nreusken@hotmail.com>
2023-02-08 23:37:43 -0800

    Solved both issues #20 and #24

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,4 +4,6 @@ create schema public;
 
 CREATE TABLE member (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), email VARCHAR(255), data jsonb);
 
-CREATE TABLE product (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), mid UUID REFERENCES member(id) ON DELETE CASCADE, data jsonb);
+CREATE TABLE category (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
+
+CREATE TABLE product (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), mid UUID REFERENCES member(id) ON DELETE CASCADE,  cid UUID REFERENCES category(id) ON DELETE CASCADE, data jsonb);
