6542dc5d82363b596406fb225ec5130df2de0b45
nreusken <nreusken@hotmail.com>
2023-02-09 14:26:32 -0800

    Removed linter errors

--- src/__tests__/pages/layout.test.tsx
+++ src/__tests__/pages/layout.test.tsx
@@ -1,6 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
-import Index from '../../pages/index';
 import Layout from "../../components/Layout";
 
 const renderView = async () => {
