0335c69cd4bde8661ef88749571b55da2ca1787e
José Chavez <josmchavz@gmail.com>
2023-02-10 15:15:21 -0800

    #23

--- src/components/product/card.tsx
+++ src/components/product/card.tsx
@@ -1,10 +1,19 @@
 import { Product } from "@/graphql/product/schema";
+import Image from "next/image";
+import {Link, Stack, Typography} from "@mui/joy";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
-    <>
-      Hello ProductCard! ProductCard goes here.
-      {product.title}
-    </>
+    <Link href={`/pages/product/${product.cid}`}>
+      <Stack className={"product-card"}>
+        <Image src={"https://via.placeholder.com/250"} alt={"Placeholder for the product image"} width={250} height={250} />
+        <Typography level={"h4"}>
+          {product.title}
+        </Typography>
+        <Typography level={"body2"}>
+          {product.price}
+        </Typography>
+      </Stack>
+    </Link>
   );
 }
