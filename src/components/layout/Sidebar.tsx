9658e623af6427703e5d1c372c5c75624379dd17
Cody Lambert <pozi@Codys-Air.attlocal.net>
2023-02-10 01:16:02 -0800

    side navigation bar setup with categories

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,7 +1,54 @@
+import { List, ListItem, ListItemButton, ListSubheader } from "@mui/joy";
+import { Box } from "@mui/system";
+
+/*
+  Currently the Category list items will be hard coded,
+  but in the future they will be dynamically generated
+  from the database if, for instance, we were to add a
+  new category to the database.
+*/
+
 export default function Sidebar() {
+
+  const categories = [
+    "Electronics", 
+    "Clothing",
+    "Sports Equipment",
+    "Toys",
+    "Furniture",
+    "Instruments",
+    "Office",
+    "Free"
+  ];
+
   return (
     <>
-      Hello Sidebar! Sidebar goes here.
+      <Box sx={{ width: "100%", height: "90%" }}>
+        <List
+          variant="outlined"
+          sx={{
+            margin: 2,
+            width: 200,
+            bgcolor: 'background.body',
+            borderRadius: 'sm',
+          }}
+        >
+          <ListItem nested>
+            <ListSubheader>
+              Shop by Category
+            </ListSubheader>
+            <List>
+              {categories.map((category) => (
+                <ListItem key={category}>
+                  <ListItemButton href="/">
+                    {category}
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </ListItem>
+        </List>
+      </Box>
     </>
   );
 }
