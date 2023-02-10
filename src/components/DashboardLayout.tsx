45c3a2c1fa31b715a878d527fdde45a6896b463d
James Tennant <jorahty@gmail.com>
2023-02-09 21:42:46 -0800

    fix dashboard layout overflow bug (#43)

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,5 +1,6 @@
-import { Divider, Stack } from "@mui/joy";
+import { Box, Divider, Stack } from "@mui/joy";
 import React from "react";
+import { headerHeight } from "./Header";
 import Sidebar from "./Sidebar";
 import SimpleLayout from "./SimpleLayout";
 
@@ -9,7 +10,13 @@ export default function DashbaordLayout({children}: {children: React.ReactNode})
       <Stack direction="row" height="100%" alignItems="flex-start" >
         <Sidebar />
         <Divider orientation="vertical"/>
-        {children}
+        <Box sx={{
+          overflowY: 'scroll',
+          height: `calc(100vh - ${headerHeight})`,
+          flexGrow: 1,
+        }}>
+          {children}
+        </Box>
       </Stack>
     </SimpleLayout>
   );
