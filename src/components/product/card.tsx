9b7f4574641076586175ba7dea032d5e7c48a141
James Tennant <jorahty@gmail.com>
2023-02-11 14:53:20 -0800

    sign in card

--- src/components/product/card.tsx
+++ src/components/product/card.tsx
@@ -1,35 +1,56 @@
 import { Product } from "@/graphql/product/schema";
-import Image from "next/image";
-import Link from "next/link";
-import {Stack, Typography} from "@mui/joy";
-import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 
-export default function ProductCard({ product }: { product: Product }) {
-  const random = 100 + Math.round(product.quantity * product.price / 20);
+import AspectRatio from '@mui/joy/AspectRatio';
+import Box from '@mui/joy/Box';
+import Card from '@mui/joy/Card';
+import IconButton from '@mui/joy/IconButton';
+import Typography from '@mui/joy/Typography';
+import Avatar from '@mui/joy/Avatar';
+import BookmarkAdd from '@mui/icons-material/BookmarkAddOutlined';
+import Link from "next/link";
+
+export default function BasicCard({ product }: { product: Product }) {
+  const random = 200 + Math.round(product.quantity * product.price / 20);
 
   return (
-    <>
-      <Link href={`/product/${product.id}`}>
-        <Stack className={"product-card"}>
-          <Image src={`https://picsum.photos/${random}`} alt={"Placeholder for the product image"} width={250} height={250} />
-          <Typography component={"h4"}>
-            {product.name}
-          </Typography>
-          <Typography level={"body2"} component={"strong"}>
-            ${product.price}
-          </Typography>
-          {/*<Typography level={"body2"}>*/}
-          {/*  {product.mid}*/}
-          {/*</Typography>*/}
-          {product.quantity <= 10 ?
-            <Typography startDecorator={<PriorityHighIcon />} textColor={"red"} level={"body3"}>
-              Only {product.quantity} left!
-            </Typography> : <></> }
-        </Stack>
+    <Card variant="outlined" sx={{ width: 320 }}>
+      <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>
+        {product.name}
+      </Typography>
+      <Typography level="body2">
+        {new Date(product.date).toLocaleDateString('en-US', {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </Typography>
+      <IconButton
+        aria-label="bookmark Bahamas Islands"
+        variant="plain"
+        color="neutral"
+        size="sm"
+        sx={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+      >
+        <BookmarkAdd />
+      </IconButton>
+      <Link href={`product/${product.id}`}>
+        <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
+          <img
+            src={`https://picsum.photos/${random}`}
+            loading="lazy"
+            alt=""
+          />
+        </AspectRatio>
       </Link>
-      <Link href={`/user/${product.user}`}>
-        {product.user}
-      </Link>
-    </>
+      <Box sx={{ display: 'flex' }}>
+        <Typography fontSize="lg" fontWeight="lg" flexGrow={1}>
+          ${product.price}
+        </Typography>
+        <Link href={`user/${product.user}`}>
+          <Avatar />
+        </Link>
+      </Box>
+    </Card>
   );
 }
