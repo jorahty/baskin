f2899023961bde8209aa4495a32a2e5f93608f3e
James Tennant <jorahty@gmail.com>
2023-02-11 02:45:11 -0800

    click user to visit user page

--- src/components/product/card.tsx
+++ src/components/product/card.tsx
@@ -5,26 +5,31 @@ import {Stack, Typography} from "@mui/joy";
 import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 
 export default function ProductCard({ product }: { product: Product }) {
-  const random = 100 + Math.round(product.quantity * product.price / 16);
+  const random = 100 + Math.round(product.quantity * product.price / 20);
 
   return (
-    <Link href={`/product/${product.id}`}>
-      <Stack className={"product-card"}>
-        <Image src={`https://picsum.photos/${random}`} alt={"Placeholder for the product image"} width={250} height={250} />
-        <Typography component={"h4"}>
-          {product.name}
-        </Typography>
-        <Typography level={"body2"} component={"strong"}>
-          ${product.price}
-        </Typography>
-        {/*<Typography level={"body2"}>*/}
-        {/*  {product.mid}*/}
-        {/*</Typography>*/}
-        {product.quantity <= 10 ?
-          <Typography startDecorator={<PriorityHighIcon />} textColor={"red"} level={"body3"}>
-            Only {product.quantity} left!
-          </Typography> : <></> }
-      </Stack>
-    </Link>
+    <>
+      <Link href={`/product/${product.id}`}>
+        <Stack className={"product-card"}>
+          <Image src={`https://picsum.photos/${random}`} alt={"Placeholder for the product image"} width={250} height={250} />
+          <Typography component={"h4"}>
+            {product.name}
+          </Typography>
+          <Typography level={"body2"} component={"strong"}>
+            ${product.price}
+          </Typography>
+          {/*<Typography level={"body2"}>*/}
+          {/*  {product.mid}*/}
+          {/*</Typography>*/}
+          {product.quantity <= 10 ?
+            <Typography startDecorator={<PriorityHighIcon />} textColor={"red"} level={"body3"}>
+              Only {product.quantity} left!
+            </Typography> : <></> }
+        </Stack>
+      </Link>
+      <Link href={`/user/${product.user}`}>
+        {product.user}
+      </Link>
+    </>
   );
 }
