c113f37c1c2705b111383e75d79ece97d65f5f7e
James Tennant <jorahty@gmail.com>
2023-02-10 18:31:18 -0800

    update list test

--- src/__tests__/frontend/components/product/list.test.tsx
+++ src/__tests__/frontend/components/product/list.test.tsx
@@ -1,19 +1,8 @@
 import { render } from '@testing-library/react'
 import ProductList from "../../../../components/product/list";
 
-const products = [{
-  mid: "string",
-  id: "string",
-  cid: "string",
-  title: "string",
-  price: 0,
-  quantity: 1,
-  description: "string",
-  date: "string",
-}];
-
 const renderView = async () => {
-  render(<ProductList products={products}/>);
+  render(<ProductList />);
 };
 
 test('Renders', async () => {
