5d61e211f44e33d51eff52c6f503d65236b2866b
James Tennant <jorahty@gmail.com>
2023-02-11 02:05:41 -0800

    category pages now functional

--- src/__tests__/frontend/pages/category.test.tsx
+++ src/__tests__/frontend/pages/category.test.tsx
@@ -1,24 +1,30 @@
-import { render } from '@testing-library/react'
 import CategoryPage from '../../../pages/category/[slug]';
+import { getServerSideProps } from '../../../pages/category/[slug]';
+import { render, screen } from '@testing-library/react'
 import { CssVarsProvider } from '@mui/joy/styles';
 import '../matchMedia';
 
 jest.mock('next/router', () => ({
   useRouter() {
     return ({
-      query: { slug: 'clothing' },
+      query: { id: '123' },
     });
   },
 }));
 
 const renderView = async () => {
+  const { props } = await getServerSideProps({
+    req: { headers: { host: 'localhost:3000' } },
+    query: { slug: 'clothing' },
+  });
   render(
     <CssVarsProvider>
-      <CategoryPage />
+      <CategoryPage products={props.products}/>
     </CssVarsProvider>
   );
 };
 
 test('Renders', async () => {
   renderView();
+  await screen.findByText('All Categories');
 });
--- src/pages/category/[slug].tsx
+++ src/pages/category/[slug].tsx
@@ -1,14 +1,22 @@
-import DashbaordLayout from '../../components/layout/DashboardLayout';
-// import { useRouter } from 'next/router';
-// import ProductList from '../../components/product/list';
+import ProductList from "../../components/product/list";
+import DashboardLayout from "../../components/layout/DashboardLayout";
+import { GetServerSideProps } from "next";
+import { Product } from "@/graphql/product/schema";
+import { ProductService } from "../../graphql/product/service";
 
-export default function CategoryPage() {
-  // const router = useRouter();
-  // const slug = router.query.slug as string;
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { slug } = query;
+  return {
+    props: {
+      products: await new ProductService().list({ category: slug as string }),
+    },
+  }
+}
 
+export default function CategoryPage({ products }: { products: Product[] }) {
   return (
-    <DashbaordLayout>
-      {/* <ProductList category={slug}/> */}
-    </DashbaordLayout>
-  );
+    <DashboardLayout>
+      <ProductList products={products}/>
+    </DashboardLayout>
+  )
 }
