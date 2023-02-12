203e618e2b81545837b34ea84d9c17ee29a4d764
Cody Lambert <codylambertdev@gmail.com>
2023-02-11 17:50:40 -0800

    Quick UI error fix

--- src/components/product/card.tsx
+++ src/components/product/card.tsx
@@ -13,44 +13,44 @@ export default function BasicCard({ product }: { product: Product }) {
   const random = 200 + Math.round(product.quantity * product.price / 20);
 
   return (
-    <Link href={`/product/${product.id}`}>
-      <Card variant="outlined" sx={{ width: 320 }}>
-        <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>
-          {product.name}
-        </Typography>
-        <Typography level="body2">
-          {new Date(product.date).toLocaleDateString('en-US', {
-            weekday: "long",
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          })}
-        </Typography>
-        <IconButton
-          aria-label="bookmark Bahamas Islands"
-          variant="plain"
-          color="neutral"
-          size="sm"
-          sx={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
-        >
-          <BookmarkAdd />
-        </IconButton>
+    <Card variant="outlined" sx={{ width: 320 }}>
+      <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>
+        {product.name}
+      </Typography>
+      <Typography level="body2">
+        {new Date(product.date).toLocaleDateString('en-US', {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </Typography>
+      <IconButton
+        aria-label="bookmark Bahamas Islands"
+        variant="plain"
+        color="neutral"
+        size="sm"
+        sx={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+      >
+        <BookmarkAdd />
+      </IconButton>
+      <Link href={`/product/${product.id}`}>
         <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
-        <img
-          src={`https://picsum.photos/${random}`}
-          loading="lazy"
-          alt=""
-        />
+          <img
+            src={`https://picsum.photos/${random}`}
+            loading="lazy"
+            alt=""
+          />
         </AspectRatio>
-        <Box sx={{ display: 'flex' }}>
-          <Typography fontSize="lg" fontWeight="lg" flexGrow={1}>
+      </Link>
+      <Box sx={{ display: 'flex' }}>
+        <Typography fontSize="lg" fontWeight="lg" flexGrow={1}>
           ${product.price}
-          </Typography>
-          <Link href={`/user/${product.user}`}>
-            <Avatar />
-          </Link>
-        </Box>
-      </Card>
-    </Link>
+        </Typography>
+        <Link href={`user/${product.user}`}>
+          <Avatar />
+        </Link>
+      </Box>
+    </Card>
   );
 }
