eb07ec9a3a48bce9ca96232c93bc9a9dc60b242a
James Tennant <jorahty@gmail.com>
2023-02-12 11:20:51 -0800

    fix lint (unused import)

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,13 +4,10 @@ import { GetServerSideProps } from "next";
 import { Product } from "@/graphql/product/schema";
 import { ProductService } from "../../graphql/product/service";
 import { Stack } from "@mui/joy";
-import {gql, GraphQLClient} from "graphql-request";
-
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const { id } = query;
   const [product] = await new ProductService().list({ id: id as string });
-
   return {
     props: {
       product: product,
