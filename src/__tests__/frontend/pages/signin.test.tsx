297609ba1119d3a8c7452af12f7760e16d29117f
Eric Hernandez <eric.hdez2001@gmail.com>
2023-02-13 01:15:50 -0800

    fixing broken 'login' queries in sign in page & tests after the renaming

--- src/__tests__/frontend/pages/signin.test.tsx
+++ src/__tests__/frontend/pages/signin.test.tsx
@@ -1,43 +1,44 @@
-import { CssVarsProvider } from '@mui/joy/styles';
-import {render, screen, waitFor, fireEvent} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {graphql} from 'msw'
-import {setupServer} from 'msw/node'
-import 'whatwg-fetch'
-import '../matchMedia';
+import { CssVarsProvider } from "@mui/joy/styles";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { graphql } from "msw";
+import { setupServer } from "msw/node";
+import "whatwg-fetch";
+import "../matchMedia";
 
-
-import Signin from '../../../pages/signin'
+import Signin from "../../../pages/signin";
 
 const handlers = [
-  graphql.query('login', async (req, res, ctx) => {
-    const json = await req.json()
-    if (json.query.indexOf('molly_member') >= 0) {
+  graphql.query("signin", async (req, res, ctx) => {
+    const json = await req.json();
+    if (json.query.indexOf("molly_member") >= 0) {
       return res(
         ctx.data({
           login: {
-            "username": "molly_member",
-            "accessToken": "whatever"
-          }
-        }),
-      )
+            username: "molly_member",
+            accessToken: "whatever",
+          },
+        })
+      );
     } else {
       return res(
-        ctx.errors ([ {
-          "message": "Unexpected error."
-        }]),
-      )
+        ctx.errors([
+          {
+            message: "Unexpected error.",
+          },
+        ])
+      );
     }
-  })
-]
+  }),
+];
 
-const server = setupServer(...handlers)
+const server = setupServer(...handlers);
 
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
-jest.mock('next/router', ()=> ({push: jest.fn()}))
+jest.mock("next/router", () => ({ push: jest.fn() }));
 
 const renderView = async () => {
   render(
@@ -47,32 +48,32 @@ const renderView = async () => {
   );
 };
 
-test('Sucess', async () => {
-  renderView()
+test("Sucess", async () => {
+  renderView();
   // // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const username = screen.getByPlaceholderText('Enter your username');
-  await userEvent.type(username, 'molly_member')
-  const passwd = screen.getByPlaceholderText('•••••••');
-  await userEvent.type(passwd, 'mollymember')
-  fireEvent.click(screen.getByLabelText('signin'))
+  const username = screen.getByPlaceholderText("Enter your username");
+  await userEvent.type(username, "molly_member");
+  const passwd = screen.getByPlaceholderText("•••••••");
+  await userEvent.type(passwd, "mollymember");
+  fireEvent.click(screen.getByLabelText("signin"));
   await waitFor(() => {
-    expect(localStorage.getItem('user')).not.toBe(null)
+    expect(localStorage.getItem("user")).not.toBe(null);
   });
 });
 
-test('Fail', async () => {
-  renderView()
-  let alerted = false
+test("Fail", async () => {
+  renderView();
+  let alerted = false;
   window.alert = () => {
-    alerted = true
-  }
-  const username = screen.getByPlaceholderText('Enter your username');
-  await userEvent.type(username, 'molly_memb')
-  const passwd = screen.getByPlaceholderText('•••••••');
-  await userEvent.type(passwd, 'mollymember')
-  fireEvent.click(screen.getByLabelText('signin'))
+    alerted = true;
+  };
+  const username = screen.getByPlaceholderText("Enter your username");
+  await userEvent.type(username, "molly_memb");
+  const passwd = screen.getByPlaceholderText("•••••••");
+  await userEvent.type(passwd, "mollymember");
+  fireEvent.click(screen.getByLabelText("signin"));
   await waitFor(() => {
-    expect(alerted).toBe(true)
-  })
-  expect(localStorage.getItem('user')).toBe(null)
+    expect(alerted).toBe(true);
+  });
+  expect(localStorage.getItem("user")).toBe(null);
 });
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,14 +1,14 @@
-import Router from 'next/router'
-import * as React from 'react';
-import { CssVarsProvider } from '@mui/joy/styles';
-import GlobalStyles from '@mui/joy/GlobalStyles';
-import CssBaseline from '@mui/joy/CssBaseline';
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import FormControl from '@mui/joy/FormControl';
-import FormLabel, { formLabelClasses } from '@mui/joy/FormLabel';
-import Input from '@mui/joy/Input';
-import Typography from '@mui/joy/Typography';
+import Router from "next/router";
+import * as React from "react";
+import { CssVarsProvider } from "@mui/joy/styles";
+import GlobalStyles from "@mui/joy/GlobalStyles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel, { formLabelClasses } from "@mui/joy/FormLabel";
+import Input from "@mui/joy/Input";
+import Typography from "@mui/joy/Typography";
 
 interface FormElements extends HTMLFormControlsCollection {
   username: HTMLInputElement;
@@ -21,17 +21,18 @@ interface SignInFormElement extends HTMLFormElement {
 
 export default function Signin() {
   React.useEffect(() => {
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
   }, []);
 
-
-  const handleSubmit = (username:string, password:string) => {
-    const query = {query: `query login{login(username: "${username}" password: "${password}") { username, accessToken }}`}
-    fetch('/api/graphql', {
-      method: 'POST',
+  const handleSubmit = (username: string, password: string) => {
+    const query = {
+      query: `query signin{signin(username: "${username}" password: "${password}") { username, accessToken }}`,
+    };
+    fetch("/api/graphql", {
+      method: "POST",
       body: JSON.stringify(query),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
       .then((res) => {
@@ -39,80 +40,77 @@ export default function Signin() {
       })
       .then((json) => {
         if (json.errors) {
-          alert('Error signing up, please try again');
+          alert("Error signing up, please try again");
         } else {
-          localStorage.setItem('user', JSON.stringify(json.data.login));
+          localStorage.setItem("user", JSON.stringify(json.data.login));
           Router.push({
-            pathname: '/'
-          })
+            pathname: "/",
+          });
         }
-      })
+      });
   };
 
   return (
-    <CssVarsProvider
-      defaultMode="dark"
-      disableTransitionOnChange
-    >
+    <CssVarsProvider defaultMode="dark" disableTransitionOnChange>
       <CssBaseline />
       <GlobalStyles
         styles={{
-          ':root': {
-            '--Collapsed-breakpoint': '769px', // form will stretch when viewport is below `769px`
-            '--Cover-width': '40vw', // must be `vw` only
-            '--Form-maxWidth': '700px',
-            '--Transition-duration': '0.4s', // set to `none` to disable transition
+          ":root": {
+            "--Collapsed-breakpoint": "769px", // form will stretch when viewport is below `769px`
+            "--Cover-width": "40vw", // must be `vw` only
+            "--Form-maxWidth": "700px",
+            "--Transition-duration": "0.4s", // set to `none` to disable transition
           },
         }}
       />
       <Box
         sx={(theme) => ({
           width:
-            'clamp(100vw - var(--Cover-width), (var(--Collapsed-breakpoint) - 100vw) * 999, 100vw)',
-          transition: 'width var(--Transition-duration)',
-          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
-          position: 'relative',
+            "clamp(100vw - var(--Cover-width), (var(--Collapsed-breakpoint) - 100vw) * 999, 100vw)",
+          transition: "width var(--Transition-duration)",
+          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+          position: "relative",
           zIndex: 1,
-          display: 'flex',
-          justifyContent: 'flex-end',
-          backdropFilter: 'blur(4px)',
-          backgroundColor: 'rgba(255 255 255 / 0.6)',
-          [theme.getColorSchemeSelector('dark')]: {
-            backgroundColor: 'rgba(19 19 24 / 0.4)',
+          display: "flex",
+          justifyContent: "flex-end",
+          backdropFilter: "blur(4px)",
+          backgroundColor: "rgba(255 255 255 / 0.6)",
+          [theme.getColorSchemeSelector("dark")]: {
+            backgroundColor: "rgba(19 19 24 / 0.4)",
           },
         })}
       >
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            minHeight: '100dvh',
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100dvh",
             width:
-              'clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)',
-            maxWidth: '100%',
+              "clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)",
+            maxWidth: "100%",
             px: 2,
           }}
         >
           <Box
             component="main"
             sx={{
-              my: 'auto',
+              my: "auto",
               py: 2,
               pb: 5,
-              display: 'flex',
-              flexDirection: 'column',
+              display: "flex",
+              flexDirection: "column",
               gap: 2,
               width: 400,
-              maxWidth: '100%',
-              mx: 'auto',
-              borderRadius: 'sm',
-              '& form': {
-                display: 'flex',
-                flexDirection: 'column',
+              maxWidth: "100%",
+              mx: "auto",
+              borderRadius: "sm",
+              "& form": {
+                display: "flex",
+                flexDirection: "column",
                 gap: 2,
               },
               [`& .${formLabelClasses.asterisk}`]: {
-                visibility: 'hidden',
+                visibility: "hidden",
               },
             }}
           >
@@ -132,26 +130,29 @@ export default function Signin() {
                   username: formElements.username.value,
                   password: formElements.password.value,
                 };
-                handleSubmit(data.username, data.password)
+                handleSubmit(data.username, data.password);
               }}
             >
               <FormControl required>
                 <FormLabel>Username</FormLabel>
-                <Input placeholder="Enter your username" type="username" name="username"/>
+                <Input
+                  placeholder="Enter your username"
+                  type="username"
+                  name="username"
+                />
               </FormControl>
               <FormControl required>
                 <FormLabel>Password</FormLabel>
-                <Input placeholder="•••••••" type="password" name="password"/>
+                <Input placeholder="•••••••" type="password" name="password" />
               </FormControl>
               <Box
                 sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
                 }}
-              >
-              </Box>
-              <Button type="submit" fullWidth aria-label='signin'>
+              ></Box>
+              <Button type="submit" fullWidth aria-label="signin">
                 Sign in
               </Button>
             </form>
@@ -160,24 +161,24 @@ export default function Signin() {
       </Box>
       <Box
         sx={(theme) => ({
-          height: '100%',
-          position: 'fixed',
+          height: "100%",
+          position: "fixed",
           right: 0,
           top: 0,
           bottom: 0,
-          left: 'clamp(0px, (100vw - var(--Collapsed-breakpoint)) * 999, 100vw - var(--Cover-width))',
+          left: "clamp(0px, (100vw - var(--Collapsed-breakpoint)) * 999, 100vw - var(--Cover-width))",
           transition:
-            'background-image var(--Transition-duration), left var(--Transition-duration) !important',
-          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
-          backgroundColor: 'background.level1',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
+            "background-image var(--Transition-duration), left var(--Transition-duration) !important",
+          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+          backgroundColor: "background.level1",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
           backgroundImage:
-            'url(https://images.unsplash.com/photo-1527181152855-fc03fc7949c8)',
-          [theme.getColorSchemeSelector('dark')]: {
+            "url(https://images.unsplash.com/photo-1527181152855-fc03fc7949c8)",
+          [theme.getColorSchemeSelector("dark")]: {
             backgroundImage:
-              'url(https://images.unsplash.com/photo-1572072393749-3ca9c8ea0831)',
+              "url(https://images.unsplash.com/photo-1572072393749-3ca9c8ea0831)",
           },
         })}
       />
