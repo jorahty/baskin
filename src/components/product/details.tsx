a8e75e6ff98b4c4838bad46bd217f5f627f2dffb
James Tennant <jorahty@gmail.com>
2023-02-15 10:45:42 -0800

    improve productDetails style

--- src/components/product/details.tsx
+++ src/components/product/details.tsx
@@ -4,7 +4,10 @@ import {
   Avatar,
   Box,
   Button,
+  Card,
+  CardOverflow,
   Chip,
+  Divider,
   Stack,
   Typography,
 } from "@mui/joy";
@@ -16,61 +19,71 @@ export default function ProductDetails({ product }: { product: Product }) {
 
   return (
     <Box maxWidth="lg" margin="auto" p={6}>
-      <Typography pb={2} level="h1">
+      <Typography pb={2} level="h2">
         {product.name}
       </Typography>
-      <Stack direction="row" gap={5}>
-        <AspectRatio ratio="1/1" sx={{ borderRadius: "xl", flexGrow: 1 }}>
-          <Image
-            alt={product.name}
-            src={`https://picsum.photos/${randomImage}`}
-            fill
-          />
-        </AspectRatio>
-        <Stack width={512} gap={2}>
-          {product.discount > 0 ? (
-            <Box>
-              <Typography level="h2">
-                {`$${(product.price - product.price * product.discount).toFixed(
-                  2
-                )} `}
-              </Typography>
-              <Typography level="h6">
-                {`Listed Price: `}
-                <Typography sx={{ textDecoration: "line-through" }}>
-                  ${product.price.toFixed(2)}
+      <Card
+        variant="outlined"
+        sx={{
+          borderRadius: 'xl',
+          gap: 4,
+          flexDirection: 'row',
+        }}
+      >
+          <CardOverflow sx={{ flexGrow: 1 }}>
+            <AspectRatio ratio="1" sx={{ borderRadius: 'xl' }}>
+              <Image
+                alt={product.name}
+                src={`https://picsum.photos/${randomImage}`}
+                fill
+              />
+            </AspectRatio>
+          </CardOverflow>
+          <Stack width={512} gap={2}>
+            {product.discount > 0 ? (
+              <Box>
+                <Typography level="h2">
+                  {`$${(product.price - product.price * product.discount).toFixed(
+                    2
+                  )} `}
                 </Typography>
-                <Typography color="danger" fontWeight="lg">
-                  {` Save ${product.discount * 100}% off!`}
+                <Typography level="h6">
+                  {`Listed Price: `}
+                  <Typography sx={{ textDecoration: "line-through" }}>
+                    ${product.price.toFixed(2)}
+                  </Typography>
+                  <Typography color="danger" fontWeight="lg">
+                    {` Save ${product.discount * 100}% off!`}
+                  </Typography>
                 </Typography>
-              </Typography>
-            </Box>
-          ) : (
-            <Typography level="h2">${product.price.toFixed(2)}</Typography>
-          )}
-          <Link href={`/user/${product.user}`}>
-            <Stack direction="row" alignItems="center" gap={1}>
-              <Avatar src={`https://robohash.org/${product.user}`} />
-              <Typography>{product.user}</Typography>
-            </Stack>
-          </Link>
-          <Stack direction="row" alignItems="center" gap={1}>
-            <Link href={`/category/${product.category}`}>
-              <Chip>{product.category}</Chip>
+              </Box>
+            ) : (
+              <Typography level="h2">${product.price.toFixed(2)}</Typography>
+            )}
+            <Link href={`/user/${product.user}`}>
+              <Stack direction="row" alignItems="center" gap={1}>
+                <Avatar src={`https://robohash.org/${product.user}`} />
+                <Typography>{product.user}</Typography>
+              </Stack>
             </Link>
-            <Typography level="body2">
-              {new Date(product.date).toLocaleDateString("en-US")}
-            </Typography>
+            <Stack direction="row" alignItems="center" gap={1}>
+              <Link href={`/category/${product.category}`}>
+                <Chip variant="soft">{product.category}</Chip>
+              </Link>
+              <Typography level="body2">
+                {new Date(product.date).toLocaleDateString("en-US")}
+              </Typography>
+            </Stack>
+            <Divider />
+            <Typography>{product.description}</Typography>
+            <Button
+              size="lg"
+              sx={{ mt: "auto", borderRadius: "lg" }}
+            >
+              Send
+            </Button>
           </Stack>
-          <Typography>{product.description}</Typography>
-          <Button
-            size="lg"
-            sx={{ mt: "auto", borderRadius: "lg" }}
-          >
-            Add to Cart
-          </Button>
-        </Stack>
-      </Stack>
+      </Card>
     </Box>
   );
 }
