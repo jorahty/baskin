36a565a2d4fea0348eae0023906f2cacd3e76f56
Cody Lambert <codylambertdev@gmail.com>
2023-02-15 22:58:23 -0800

    Sorted product list by date and price (#92)

--- src/__tests__/frontend/components/product/list.test.tsx
+++ src/__tests__/frontend/components/product/list.test.tsx
@@ -1,17 +1,31 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from "@testing-library/react";
 import ProductList from "../../../../components/product/list";
 
-const products = [{
-  user: "string",
-  id: "string",
-  category: "string",
-  name: "string",
-  price: 0,
-  quantity: 1,
-  description: "string",
-  date: "string",
-  pictures: ["string"]
-}];
+const products = [
+  {
+    user: "string",
+    id: "string",
+    category: "string",
+    name: "string",
+    price: 0,
+    quantity: 1,
+    description: "string",
+    date: new Date().toISOString(),
+    pictures: ["string"],
+    discount: 0
+  },
+  {
+    user: "string",
+    id: "string",
+    category: "string",
+    name: "string",
+    price: 1,
+    quantity: 1,
+    description: "string",
+    date: new Date().toISOString(),
+    pictures: ["string"],
+    discount: 0
+  }];
 
 const renderView = async () => {
   render(<ProductList products={products}/>);
@@ -20,4 +34,17 @@ const renderView = async () => {
 test('Renders', async () => {
   renderView();
 });
+
+test('Sorting', async () => {
+  renderView();
+  fireEvent.click(screen.getByText("sort products"));
+  fireEvent.click(screen.getByText("Newest"));
+  fireEvent.click(screen.getByText("sort products"));
+  fireEvent.click(screen.getByText("Oldest"));
+  fireEvent.click(screen.getByText("sort products"));
+  fireEvent.click(screen.getByText("Price High"));
+  fireEvent.click(screen.getByText("sort products"));
+  fireEvent.click(screen.getByText("Price Low"));
+});
+
 
