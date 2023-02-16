e0b5a65106815a0c25a4f281b1f9807992fd85f4
ashjyaw <ashjyaw1@gmail.com>
2023-02-16 15:57:21 -0800

    refactor 2

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
