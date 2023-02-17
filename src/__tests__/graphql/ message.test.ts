a1336c7ef4e6c12d075d0f359fca41e051fed965
ashjyaw <ashjyaw1@gmail.com>
2023-02-16 22:51:54 -0800

    refined testing for new data

--- src/__tests__/graphql/ message.test.ts
+++ src/__tests__/graphql/ message.test.ts
@@ -24,12 +24,26 @@ afterAll((done) => {
   db.shutdown();
 });
 
-test("Fetch messages by convo id", async () => {
+test("Fetch messages by convo id anna and molly", async () => {
   await request
     .post("/api/graphql")
     .send({
       query: `{
             message(id: "f94a1252-7d5e-4b87-ae41-7a03f58a4028" ){id}
+          }`,
+    })
+    .expect(200)
+    .then((res) => {
+      expect(res).toBeDefined();
+    });
+});
+
+test("Fetch messages by convo id mia and nobby ", async () => {
+  await request
+    .post("/api/graphql")
+    .send({
+      query: `{
+            message(id: "52d3f9fd-1c08-41cc-973e-2d2c0d75f4b4" ){id}
           }`,
     })
     .expect(200)
