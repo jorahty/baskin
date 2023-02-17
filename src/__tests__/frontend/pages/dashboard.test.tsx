f481ee3e4e673c7362a810b1fad31b24b7032d12
Cody Lambert <codylambertdev@gmail.com>
2023-02-16 19:38:08 -0800

    Added the page for the dashboard, still in progress (#134)

---
+++ src/__tests__/frontend/pages/dashboard.test.tsx
@@ -0,0 +1,17 @@
+import {render, screen} from "@testing-library/react";
+import {CssVarsProvider} from "@mui/joy/styles";
+import Dashboard from "../../../pages/dashboard";
+import "../matchMedia";
+
+const renderView = async () => {
+  render(
+    <CssVarsProvider>
+      <Dashboard />
+    </CssVarsProvider>
+  );
+};
+
+test('Renders', async () => {
+  renderView();
+  await screen.findByText('Dashboard View');
+});
