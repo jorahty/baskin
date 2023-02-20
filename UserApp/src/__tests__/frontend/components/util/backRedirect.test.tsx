831e6731e1e32f0594a2a1a3e5472bab4c073f1e
José Chavez <josmchavz@gmail.com>
2023-02-19 21:41:56 -0800

    #154

---
+++ UserApp/src/__tests__/frontend/components/util/backRedirect.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import BackRedirect from "../../../../components/util/BackRedirect";
+import Router from "next/router";
+
+jest.mock("next/router", () => ({
+  push: jest.fn(),
+  back: jest.fn(),
+}));
+
+const renderView = async (url?: string) => {
+  render(<BackRedirect url={url} />);
+};
+
+test("Renders", async () => {
+  await renderView();
+  const button = await screen.findByLabelText("back");
+  await button.click();
+});
+
+test("Renders with link", async () => {
+  await renderView("/dashboard");
+  const button = await screen.findByLabelText("back");
+  button.click();
+  expect(Router.push).toHaveBeenCalledWith("/dashboard");
+});
--- UserApp/src/__tests__/frontend/pages/signup.test.tsx
+++ UserApp/src/__tests__/frontend/pages/signup.test.tsx
@@ -1,23 +1,23 @@
-import { CssVarsProvider } from '@mui/joy/styles';
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { graphql } from 'msw';
-import { setupServer } from 'msw/node';
-import 'whatwg-fetch';
-import '../matchMedia';
+import { CssVarsProvider } from "@mui/joy/styles";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { graphql } from "msw";
+import { setupServer } from "msw/node";
+import "whatwg-fetch";
+import "../matchMedia";
 
-import Signup from '../../../pages/signup';
-import { AppContextProvider } from '../../../context';
+import Signup from "../../../pages/signup";
+import { AppContextProvider } from "../../../context";
 
 const handlers = [
-  graphql.mutation('addUser', async (req, res, ctx) => {
+  graphql.mutation("addUser", async (req, res, ctx) => {
     const json = await req.json();
-    if (json.query.indexOf('john@doe.com') >= 0) {
+    if (json.query.indexOf("john@doe.com") >= 0) {
       return res(
         ctx.data({
           signUp: {
-            name: 'John Doe',
-            email: 'john@doe.com',
+            name: "John Doe",
+            email: "john@doe.com",
           },
         })
       );
@@ -25,7 +25,7 @@ const handlers = [
       return res(
         ctx.errors([
           {
-            message: 'Unexpected error.',
+            message: "Unexpected error.",
           },
         ])
       );
@@ -39,7 +39,7 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-jest.mock('next/router', () => ({ push: jest.fn() }));
+jest.mock("next/router", () => ({ push: jest.fn() }));
 
 const renderView = () => {
   render(
@@ -51,52 +51,52 @@ const renderView = () => {
   );
 };
 
-test('Sucess', async () => {
+test("Sucess", async () => {
   renderView();
   let alerted = false;
   window.alert = () => {
     alerted = true;
   };
-  const first = screen.getByPlaceholderText('Enter your first name');
-  await userEvent.type(first, 'john');
-  const last = screen.getByPlaceholderText('Enter your last name');
-  await userEvent.type(last, 'does');
-  const email = screen.getByPlaceholderText('Enter your email');
-  await userEvent.type(email, 'john@doe.com');
-  const username = screen.getByPlaceholderText('Enter your username');
-  await userEvent.type(username, 'john_doe');
-  const passwd = screen.getByPlaceholderText('•••••••');
-  await userEvent.type(passwd, 'johndoes');
-  fireEvent.click(screen.getByLabelText('signup'));
+  const first = screen.getByPlaceholderText("Enter your first name");
+  await userEvent.type(first, "john");
+  const last = screen.getByPlaceholderText("Enter your last name");
+  await userEvent.type(last, "does");
+  const email = screen.getByPlaceholderText("Enter your email");
+  await userEvent.type(email, "john@doe.com");
+  const username = screen.getByPlaceholderText("Enter your username");
+  await userEvent.type(username, "john_doe");
+  const passwd = screen.getByPlaceholderText("•••••••");
+  await userEvent.type(passwd, "johndoes");
+  fireEvent.click(screen.getByLabelText("signup"));
   await waitFor(() => {
     expect(alerted).toBe(false);
   });
 });
 
-test('Fail', async () => {
+test("Fail", async () => {
   renderView();
   let alerted = false;
   window.alert = () => {
     alerted = true;
   };
-  const first = screen.getByPlaceholderText('Enter your first name');
-  await userEvent.type(first, 'molly');
-  const last = screen.getByPlaceholderText('Enter your last name');
-  await userEvent.type(last, 'member');
-  const email = screen.getByPlaceholderText('Enter your email');
-  await userEvent.type(email, 'mollymember@books.com');
-  const username = screen.getByPlaceholderText('Enter your username');
-  await userEvent.type(username, 'molly_memb');
-  const passwd = screen.getByPlaceholderText('•••••••');
-  await userEvent.type(passwd, 'mollymember');
-  fireEvent.click(screen.getByLabelText('signup'));
+  const first = screen.getByPlaceholderText("Enter your first name");
+  await userEvent.type(first, "molly");
+  const last = screen.getByPlaceholderText("Enter your last name");
+  await userEvent.type(last, "member");
+  const email = screen.getByPlaceholderText("Enter your email");
+  await userEvent.type(email, "mollymember@books.com");
+  const username = screen.getByPlaceholderText("Enter your username");
+  await userEvent.type(username, "molly_memb");
+  const passwd = screen.getByPlaceholderText("•••••••");
+  await userEvent.type(passwd, "mollymember");
+  fireEvent.click(screen.getByLabelText("signup"));
   await waitFor(() => {
     expect(alerted).toBe(true);
   });
 });
 
-test('Sucess', async () => {
-  renderView();
-  const backButton = screen.getByLabelText('back');
-  await backButton.click();
-});
+// test('Sucess', async () => {
+//   renderView();
+//   const backButton = screen.getByLabelText('back');
+//   await backButton.click();
+// });
