541006227cb6ad6ca99c7b6d484ce63494855b79
James Tennant <jorahty@gmail.com>
2023-02-21 09:32:55 -0800

    format SQL

--- UserApp/sql/data.sql
+++ UserApp/sql/data.sql
@@ -34,13 +34,9 @@ INSERT INTO favorite (member_username, product_id) VALUES ('molly_member', '035f
 INSERT INTO favorite (member_username, product_id) VALUES ('anna_admin', '038b7e70-a5c0-47e6-80f3-5b1772bb4a0d');
 INSERT INTO favorite (member_username, product_id) VALUES ('anna_admin', '5c55a266-a631-4598-9bd5-52bd5ee2d9aa');
 
--- chat
+-- Put Molly and Anna into a chat together ("direct message")
 INSERT INTO chat (id, data) VALUES ('f94a1252-7d5e-4b87-ae41-7a03f58a4028', '{"name": "null"}');
-INSERT INTO chat (id, data) VALUES ('52d3f9fd-1c08-41cc-973e-2d2c0d75f4b4', '{"name": "Hiking"}');
-INSERT INTO chat (id, data) VALUES ('28a7f960-c779-4b27-b487-fa55264d79d3', '{"name": null}');
-INSERT INTO chat (id, data) VALUES ('3f37a49c-7d13-4764-9568-8c5e13050b68', '{"name": "Samsung TV"}');
 
--- Put Molly and Anna into a chat together
 INSERT INTO chat_member(member_username,chat_id) VALUES('molly_member', 'f94a1252-7d5e-4b87-ae41-7a03f58a4028');
 INSERT INTO chat_member(member_username,chat_id) VALUES('anna_admin', 'f94a1252-7d5e-4b87-ae41-7a03f58a4028');
 
@@ -48,6 +44,8 @@ INSERT INTO message (chat_id, data) VALUES ('f94a1252-7d5e-4b87-ae41-7a03f58a402
 INSERT INTO message (chat_id, data) VALUES ('f94a1252-7d5e-4b87-ae41-7a03f58a4028', '{"sender": "anna_admin", "date": "2023-01-21T15:21:08.000Z", "content": "Hi Molly!"}');
 
 -- Put Molly, Anna, and Mia into a "group" chat together
+INSERT INTO chat (id, data) VALUES ('52d3f9fd-1c08-41cc-973e-2d2c0d75f4b4', '{"name": "Hiking"}');
+
 INSERT INTO chat_member(member_username, chat_id) VALUES('molly_member', '52d3f9fd-1c08-41cc-973e-2d2c0d75f4b4');
 INSERT INTO chat_member(member_username, chat_id) VALUES('anna_admin', '52d3f9fd-1c08-41cc-973e-2d2c0d75f4b4');
 INSERT INTO chat_member(member_username, chat_id) VALUES('mia_moderator', '52d3f9fd-1c08-41cc-973e-2d2c0d75f4b4');
@@ -56,7 +54,9 @@ INSERT INTO message (chat_id, data) VALUES ('52d3f9fd-1c08-41cc-973e-2d2c0d75f4b
 INSERT INTO message (chat_id, data) VALUES ('52d3f9fd-1c08-41cc-973e-2d2c0d75f4b4', '{"sender": "anna_admin", "date": "2023-01-21T15:25:08.000Z", "content": "The thermos is with me"}');
 INSERT INTO message (chat_id, data) VALUES ('52d3f9fd-1c08-41cc-973e-2d2c0d75f4b4', '{"sender": "molly_member", "date": "2023-01-21T15:26:08.000Z", "content": "Hope we cross paths with the coyotes again"}');
 
--- Put Molly, Anna, and Mia into an unnamed "group" chat together
+-- Put Molly, Anna, and Mia into an unnamed group chat together
+INSERT INTO chat (id, data) VALUES ('28a7f960-c779-4b27-b487-fa55264d79d3', '{"name": null}');
+
 INSERT INTO chat_member(member_username, chat_id) VALUES('molly_member', '28a7f960-c779-4b27-b487-fa55264d79d3');
 INSERT INTO chat_member(member_username, chat_id) VALUES('anna_admin', '28a7f960-c779-4b27-b487-fa55264d79d3');
 INSERT INTO chat_member(member_username, chat_id) VALUES('mia_moderator', '28a7f960-c779-4b27-b487-fa55264d79d3');
@@ -66,6 +66,8 @@ INSERT INTO message (chat_id, data) VALUES ('28a7f960-c779-4b27-b487-fa55264d79d
 INSERT INTO message (chat_id, data) VALUES ('28a7f960-c779-4b27-b487-fa55264d79d3', '{"sender": "molly_member", "date": "2023-01-21T15:16:08.000Z", "content": "Hello"}');
 
 -- Put Molly and Mia into a chat about "Samsung TV"
+INSERT INTO chat (id, data) VALUES ('3f37a49c-7d13-4764-9568-8c5e13050b68', '{"name": "Samsung TV"}');
+
 INSERT INTO chat_member(member_username,chat_id) VALUES('molly_member', '3f37a49c-7d13-4764-9568-8c5e13050b68');
 INSERT INTO chat_member(member_username,chat_id) VALUES('mia_moderator', '3f37a49c-7d13-4764-9568-8c5e13050b68');
 
