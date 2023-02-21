30631db41a2cc0284d0af0d06187ab9d2ac33bb9
José Chavez <josmchavz@gmail.com>
2023-02-21 12:14:28 -0800

    #155

---
+++ UserApp/src/components/dashboard/ProductMenu.tsx
@@ -0,0 +1,75 @@
+import { Button, Container, Stack } from '@mui/joy';
+import Typography from '@mui/joy/Typography';
+import { AddCircle } from '@mui/icons-material';
+import { useAppContext } from '../../context';
+import { useEffect, useState } from 'react';
+import { Product } from '@/graphql/product/schema';
+import { gql, GraphQLClient } from 'graphql-request';
+import ProductTable from '../../components/dashboard/ProductTable';
+
+export default function ProductMenu() {
+  const { signedInUser } = useAppContext();
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const graphQLClient = new GraphQLClient('http://localhost:3000/api/graphql', {
+        headers: {
+          Authorization: `Bearer ${signedInUser?.accessToken}`,
+        },
+      });
+
+      const query = gql`
+        query getAllProducts($username: String!) {
+          product(user: $username) {
+            price
+            pictures
+            date
+            description
+            name
+            category
+            quantity
+            discount
+            id
+          }
+        }
+      `;
+
+      console.log('signedInUser', signedInUser?.username);
+
+      const data = await graphQLClient.request(query, {
+        username: `${signedInUser?.username}`,
+      });
+      setProducts(data.product);
+    };
+
+    fetchData();
+  }, [signedInUser]);
+
+  return (
+    <Container sx={{ margin: '16px auto' }}>
+      <Typography
+        component="h1"
+        sx={{
+          fontSize: '2rem',
+          fontWeight: 'bold',
+          color: 'primary',
+          mb: 4,
+        }}
+      >
+        Products
+      </Typography>
+      <Stack>
+        <Button
+          startDecorator={<AddCircle />}
+          sx={{
+            width: { md: 'fit-content' },
+          }}
+        >
+          Add Product
+        </Button>
+        <ProductTable products={products} />
+      </Stack>
+    </Container>
+  );
+}
---
+++ UserApp/src/components/dashboard/ProductTable.tsx
@@ -0,0 +1,65 @@
+import { Button, Sheet, Table, Typography } from '@mui/joy';
+import { Product } from '@/graphql/product/schema';
+import Image from 'next/image';
+
+// Reference: https://codesandbox.io/s/6bmeke?file=/components/OrderTable.tsx:7018-12425
+
+export default function ProductTable({ products }: { products: Product[] }) {
+  return (
+    <Sheet
+      className="OrderTableContainer"
+      variant="outlined"
+      sx={{
+        width: '100%',
+        borderRadius: 'md',
+        flex: 1,
+        overflow: 'auto',
+        minHeight: 0,
+        margin: '20px 0',
+      }}
+    >
+      <Table
+        aria-labelledby="tableTitle"
+        stickyHeader
+        hoverRow
+        sx={{
+          '--TableCell-headBackground': theme => theme.vars.palette.background.level1,
+          '--Table-headerUnderlineThickness': '1px',
+          '--TableRow-hoverBackground': theme => theme.vars.palette.background.level1,
+        }}
+      >
+        <thead>
+          <tr>
+            <th style={{ width: 140, padding: 12 }}>Image</th>
+            <th style={{ width: 220, padding: 12 }}>Product</th>
+            <th style={{ width: 160, padding: 12 }}>Category</th>
+            <th style={{ width: 120, padding: 12 }}>Price</th>
+            <th style={{ width: 100, padding: 12 }}></th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((row: Product) => (
+            <tr key={row.id}>
+              <td>
+                <Image
+                  src={row.pictures[0]}
+                  alt={`Product image of ${row.name}`}
+                  width={100}
+                  height={100}
+                />
+              </td>
+              <td>
+                <Typography fontWeight="md">{row.name}</Typography>
+              </td>
+              <td>{row.category}</td>
+              <td>{row.price}</td>
+              <td>
+                <Button>Edit</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Sheet>
+  );
+}
