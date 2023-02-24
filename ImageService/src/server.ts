334e401660877a052f7e224cf3e9d6bc9d99a63e
James Tennant <jorahty@gmail.com>
2023-02-24 09:45:58 -0800

    ImageService: print link to testing ui, add title to testing ui

--- ImageService/src/server.ts
+++ ImageService/src/server.ts
@@ -3,5 +3,6 @@ import app from './app';
 const port = 3012;
 
 app.listen(port, () => {
-  console.log(`Server Running at http://localhost:${port}`);
+  console.log(`Server Running on port ${port}`);
+  console.log(`API Testing UI: http://localhost:${port}/api-docs`);
 });
