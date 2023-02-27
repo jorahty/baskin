b50383c902d228eb657852511f55b39103e32fd1
James Tennant <jorahty@gmail.com>
2023-02-27 15:31:46 -0800

    UserApp: remove product and favorite tables

--- UserApp/sql/schema.sql
+++ UserApp/sql/schema.sql
@@ -1,12 +1,6 @@
 DROP SCHEMA public cascade;
 CREATE SCHEMA public;
 
--- CREATE TABLE member (username VARCHAR(32) PRIMARY KEY NOT NULL, data jsonb, UNIQUE(username));
-
-CREATE TABLE product (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), member_username VARCHAR(32), category_slug VARCHAR(32), data jsonb);
-
-CREATE TABLE favorite (member_username VARCHAR(32), product_id UUID REFERENCES product(id) ON DELETE CASCADE, UNIQUE (member_username, product_id));
-
 CREATE TABLE chat (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
 
 CREATE TABLE message (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), chat_id UUID REFERENCES chat(id), data jsonb);
