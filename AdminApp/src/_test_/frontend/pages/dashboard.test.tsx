0939454bc2f986dae241164602b124e7af5808bf
nreusken <nreusken@hotmail.com>
2023-02-28 22:23:33 -0800

    Sidebar(Admin side menu #189)

--- AdminApp/src/_test_/frontend/pages/dashboard.test.tsx
+++ AdminApp/src/_test_/frontend/pages/dashboard.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { CssVarsProvider } from '@mui/joy/styles';
 import '../matchMedia';
 
@@ -14,4 +14,23 @@ const renderView = async () => {
 
 test('Render page', async () => {
   await renderView();
+});
+
+test('Clicks on sidebar', async () => {
+  renderView();
+  const sidebar = await screen.findByText('Categories');
+  fireEvent.click(sidebar);
+});
+
+
+test('Close User Menu', async () => {
+  // this test is kinda scuffed. I spent so long trying to make it disappear.
+  // The only way I got it to close was by clicking a button on the menu.
+  // Clicking away or on other elements NEVER triggered its handleClose()
+  renderView();
+  fireEvent.click(screen.getByText('Welcome to the dashboard'));
+  const sidebar = await screen.findByText('Categories');
+  fireEvent.click(sidebar);
+  expect(screen.queryByText('Welcome to the dashboard')).not.toBeInTheDocument();
+  fireEvent.click(screen.getByText('Welcome to the Categories'));
 });
