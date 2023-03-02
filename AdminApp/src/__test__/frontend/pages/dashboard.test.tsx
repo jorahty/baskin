fb9b31dd8f2dc22ce51b8ff29632fdeebba5fac2
nreusken <nreusken@hotmail.com>
2023-03-01 23:05:55 -0800

    Edited test

--- AdminApp/src/__test__/frontend/pages/dashboard.test.tsx
+++ AdminApp/src/__test__/frontend/pages/dashboard.test.tsx
@@ -1,8 +1,36 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { CssVarsProvider } from '@mui/joy/styles';
 import '../matchMedia';
+import { setupServer } from 'msw/node';
+import { graphql } from 'msw';
 
 import Dashboard from '../../../pages/dashboard';
+
+
+const handlers = [
+  graphql.query('getAllCategories', async (req, res, ctx) => {
+    return res(
+      ctx.data({
+        category: [
+          {
+            name: 'Vehicles',
+            slug: 'vehicles',
+          },
+        ],
+      }),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
 
 const renderView = async () => {
   render(
