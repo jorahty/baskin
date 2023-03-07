900f27ef1529009bf7584dee21aa870c9573803e
James Tennant <jorahty@gmail.com>
2023-03-07 01:42:08 -0800

    ProductService: create attribute table

--- ProductService/sql/schema.sql
+++ ProductService/sql/schema.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS attribute;
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS category;
 
 CREATE TABLE category (slug VARCHAR(32) PRIMARY KEY NOT NULL, parent_slug VARCHAR(32) REFERENCES category(slug) ON DELETE CASCADE ON UPDATE CASCADE, data jsonb, UNIQUE(slug));
 
 CREATE TABLE product (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), member_username VARCHAR(32), category_slug VARCHAR(32) REFERENCES category(slug) ON DELETE CASCADE ON UPDATE CASCADE, data jsonb);
+
+CREATE TABLE attribute (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), category_slug VARCHAR(32) REFERENCES category(slug) ON DELETE CASCADE, data jsonb, UNIQUE (id));
