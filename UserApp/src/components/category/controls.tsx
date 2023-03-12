d068a393b06344b73a549bdba9a1214fd0ab1391
James Tennant <jorahty@gmail.com>
2023-03-11 16:18:22 -0800

    Remove debug info

--- UserApp/src/components/category/controls.tsx
+++ UserApp/src/components/category/controls.tsx
@@ -42,9 +42,6 @@ export default function CategoryControls({ category }: Props) {
 
   return (
     <Stack p={2} gap={2} width={340}>
-      {refinement.search === 'debug' && category.attributes.map(attribute => (
-        <div key={attribute.id}>id: {attribute.id}, name: {attribute.name}</div>
-      ))}
       <Link href="/product/create">
         <Button fullWidth>{t('createNewProduct.title')}</Button>
       </Link>
