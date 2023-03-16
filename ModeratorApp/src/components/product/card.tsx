83748948d42a5090f1b8a68ab222e2adf8278c51
ashjyaw <ashjyaw1@gmail.com>
2023-03-15 20:08:52 -0700

    ModeratorApp added delete button not functional yet

--- ModeratorApp/src/components/product/card.tsx
+++ ModeratorApp/src/components/product/card.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/joy';
 import { Product } from '../../../../ProductService/src/product/schema';
 
 interface Props{
@@ -5,10 +6,16 @@ interface Props{
 }
 
 export default function ProductCard({ product }: Props){
+
   return (
     <tr>
       <td>{product.name}</td>
       <td>{product.user}</td>
+      <td>
+        <Button size="sm" variant="soft" color="danger">
+          Delete
+        </Button>
+      </td>
     </tr>
   );
 }
